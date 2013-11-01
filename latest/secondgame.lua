8eb486b9fdd6d9d2ea8f587239b6a00b246d13a0
Maricia <mariciabalayan@yahoo.com>
2013-11-01 08:56:14 +0800

    Playable 2nd game

--- latest/secondgame.lua
+++ latest/secondgame.lua
@@ -3,9 +3,20 @@ local storyboard = require ("storyboard")
 local widget = require( "widget" )
 local timer = require("timer")
 local scene = storyboard.newScene()
-local gridView = require("gridView")
 local path = system.pathForFile("JaVaMiaDb.sqlite3", system.ResourceDirectory)
 db = sqlite3.open( path )   
+
+local numberOfCategories
+local gameCategories
+local images
+local word
+local categories
+local length
+local correctWords
+local answers
+local incorrectSound = audio.loadSound("incorrect.mp3")
+local correctSound = audio.loadSound("correct.mp3")
+local currScore = 0
 ------- Global variables ---------
 
 function _destroyDialog()
@@ -14,8 +25,8 @@ end
 
 function scene:createScene(event)
 
-	local categories = {"Living", "Non-Living", "Red", "Green", "Blue", "Yellow", "Triangle", "Rectangle", "Circle", "Animal", "Body Part"}
-	local values = {"1", "0", "red", "green", "blue", "yellow", "triangle", "rectangle", "circle", "1", "1"}
+	categories = {"Living", "Non-Living", "Red", "Green", "Blue", "Yellow", "Triangle", "Rectangle", "Circle", "Animal", "Body Part"}
+	values = {"1", "0", "red", "green", "blue", "yellow", "triangle", "rectangle", "circle", "1", "1"}
 
 	level = event.params.categ
 	screenGroup = self.view
@@ -25,53 +36,96 @@ function scene:createScene(event)
 
 	--HEADER
 	score = display.newText("SCORE: ", -30, 0, font, 20)
-	timer = display.newText("TIME: ", 400, 0, font, 20)
+	scoreNumber = display.newText(currScore, 50, 0, font, 20)
+	-- Display score
+	timerLabel = display.newText("TIME: ", 400, 0, font, 20)
 	screenGroup:insert(score)
-	screenGroup:insert(timer)
+	screenGroup:insert(timerLabel)
 
-	--BOXES
-	boxGroup = display.newGroup()
-	boxSize = 50
-	box1 = display.newImageRect("images/secondgame/box.png", 80, 80)
-	box2 = display.newImageRect("images/secondgame/box.png", 80, 80)
+	timerText = display.newText( "0:00", 460, 0, font, 20 )
+	function timerText:timer( event )
+	   	maxTime = maxTime-1
+		if (maxTime % 60 < 10) then timerText.text = math.floor(maxTime/60) .. ":0" .. (maxTime%60)
+		else timerText.text = math.floor(maxTime/60) .. ":" .. (maxTime%60)
+		end
 
+	    if(maxTime == 0)then
+			timer.cancel( event.source )
+			_destroyDialog()
+			print("TIME'S UP!")
+	    end
+	end
+
+	timeDelay = 1000
+	maxTime = 60
+	timerText.text = maxTime/60 .. ":00"
+	gameTimer = timer.performWithDelay( timeDelay, timerText, maxTime )	-- maintain time kahit magreload na
+
+	-- FUNCTION FOR GETTING WORDS FROM DB
 	local function getWords(type)
 		dbFields = {}
 		dbValues = {}
-		for i = 1, #rand do
-			if rand[i] == 1 or rand[i] == 2 then
+		for i = 1, #gameCategories do
+			if gameCategories[i] == 1 or gameCategories[i] == 2 then
 				dbFields[i] = "livingThingCategory"
-				dbValues[i] = values[rand[i]]
-			elseif rand[i] >= 3 and rand[i] <= 6 then
+				dbValues[i] = values[gameCategories[i]]
+			elseif gameCategories[i] >= 3 and gameCategories[i] <= 6 then
 				dbFields[i] = "colorCategory"
-				dbValues[i] = values[rand[i]]
-			elseif rand[i] >= 7 and rand[i] <= 9 then
+				dbValues[i] = values[gameCategories[i]]
+			elseif gameCategories[i] >= 7 and gameCategories[i] <= 9 then
 				dbFields[i] = "shapeCategory" 
-				dbValues[i] = values[rand[i]]			
-			elseif rand[i] == 10 then
+				dbValues[i] = values[gameCategories[i]]			
+			elseif gameCategories[i] == 10 then
 				dbFields[i] = "animalCategory"
-				dbValues[i] = values[rand[i]]
-			elseif rand[i] == 11 then
+				dbValues[i] = values[gameCategories[i]]
+			elseif gameCategories[i] == 11 then
 				dbFields[i] = "bodyPartCategory"
-				dbValues[i] = values[rand[i]]				
+				dbValues[i] = values[gameCategories[i]]				
 			end
 		end
 
 		--query database:correct words
+
+		answers = {}
 		correctWords = {}
-		print("ALL WORDS CORRECT:")
 		for i = 1, #dbFields do
-			print("---FOR"..dbFields[i].."---")
+		    answers[i] = {}
+		    j = 1
 			for row in db:nrows("SELECT * FROM Words where "..dbFields[i].." = '".. dbValues[i] .. "'") do
-				correctWords[#correctWords+1] = row.name
-				print(row.name)
+				answers[i][j] = row.name
+				j = j + 1
+			end
+		end
+
+		-- all correct answers, no duplicates
+		correctWords[1] = answers[1][1]
+		for i = 1, #dbFields do
+			for j = 1, 127 do
+				isUnique = true
+				for k = 1, #correctWords do
+					if answers[i][j] == correctWords[k] then
+						isUnique = false
+					end
+				end
+
+				if isUnique == true and answers[i][j] ~= nil then
+					correctWords[#correctWords+1] = answers[i][j]
+				end
+			end
+		end
+
+		for i = 1, #dbFields do
+			print("!!!!!!"..dbFields[i])
+			for j = 1, 127 do
+				if answers[i][j] ~= nil then
+					print(answers[i][j])
+				end
 			end
 		end
 
 		if type == "correct" then
 			words = correctWords
 			print("TOTAL CORRECT"..#words)
-
 		--query database:extra words
 		elseif type == "incorrect" then
 			words = {}
@@ -109,11 +163,12 @@ function scene:createScene(event)
 		return words
 	end
 
-	--Process the event when user click on the grid
-	local function gridListener(index)
---		print("You select item "..index)
+	local function updateScore()
+		currScore = currScore + 1
+		scoreNumber.text = currScore
 	end
 
+	-- FUNCTION FOR RANDOMIZING A CATEGORY
 	local function randomizeCategory()
 		rand = {}
 		rand[1] = math.random(#categories)
@@ -128,145 +183,271 @@ function scene:createScene(event)
 		return rand
 	end
 
+	function shuffle(array)
+		for i = 1, #array*2 do -- repeat this for twice the amount of elements in the table, to make sure everything is shuffled well
+			a = math.random(#array)
+			b = math.random(#array)
+			array[a], array[b] = array[b], array[a]
+		end
+
+		return array
+	end
+
+	-- FUNCTION FOR SELECTING N WORDS
 	local function randomize(array, limit)
-		words = {}
-		words[1] = array[math.random(#array)]
-		for i = 2, limit do
-			words[i] = array[math.random(#array)]
-			for j = 1, i-1 do
-				while(words[i] == words[j]) do
-					words[i] = array[math.random(#array)]
-				end
-			end
+
+		print("CORRECT ANSWERS:")
+		for i = 1, #array do
+			print(array[i])
+		end
+
+		-- shuffle entries
+		array = shuffle(array)
+		print("SHUFFLED ANSWERS:")
+		for i = 1, #array do
+			print(array[i])
+		end
+
+		for i = 1, limit do
+			words[i] = array[i]
 		end
 
 		return words
 	end
 
+	-- FUNCTION FOR DRAGGING IMAGES
+	local function imageDrag (event)
+		local imagePosX = {}
+		local imagePosY = {}
+		isMoved = false
+		t = event.target
+
+		if event.phase == "moved" or event.phase == "ended" then
+			for i =1, numberOfCategories do
+				imagePosX[i] = math.abs(t.x - box[i].x)
+				imagePosY[i] = math.abs(t.y - box[i].y)
+			end
+			-- Snap to middle
+			for i = 1, numberOfCategories do
+				if (imagePosX[i] <= 50) and (imagePosY[i] <= 50) then
+					t.x = box[i].x;
+					t.y = box[i].y;
+					isMoved = true
+				end
+			end
+		end
+
+		if event.phase == "ended" then
+			-- Check answer
+			if isMoved == true then
+				for i = 1, numberOfCategories do
+					if t.x == box[i].x then
+						boxNumber = i
+						print("box# "..boxNumber)
+						break
+					end
+				end
+
+				for i = 1, #images do
+					if t == images[i] then
+						wordToCheck = word[i]
+						print(wordToCheck)
+						break
+					end
+				end
+
+				isCorrect = false
+				for j = 1, 127 do
+					if answers[boxNumber][j] == wordToCheck then
+						print("CORRECT!")
+						updateScore()
+						print("SCORE:"..currScore)
+						isCorrect = true
+						audio.play(correctSound)
+						t:removeSelf()
+						break
+					end
+				end
+				if isCorrect == false then
+					print("WRONG!")
+					audio.play(incorrectSound)
+					t.x = t.initialX
+					t.y = t.initialY
+				end
+			end
+
+		end
+
+		return true
+	end 
 
 	----------------------------------------------------------------Data
-	easy = {}; med = {}; hard = {}
-	easyText = {}; medText = {}; hardText = {}
+
+	function drawGrid(gridX, gridY, photoArray, photoTextArray, columnNumber, paddingX, paddingY, photoWidth, photoHeight)
+
+		local currentX = gridX
+		local currentY = gridY
+		images = {}
+
+		for i = 1, #photoArray do
+			local fontSize = 12
+
+			images[i] = display.newImageRect(photoArray[i], photoWidth, photoHeight)
+			images[i].x = currentX + 23
+			images[i].y = currentY + 20
+			images[i].initialX = images[i].x
+			images[i].initialY = images[i].y
+
+			local textPosX = photoWidth/2 - (fontSize/2)*string.len(photoTextArray[i])/2
+			textObject = display.newText( photoTextArray[i], currentX + textPosX, currentY + photoHeight - 50, native.systemFontBold, fontSize )
+			textObject:setTextColor( 255,255,255 )
+
+			--Update the position of the next item
+			currentX = currentX + photoWidth + paddingX
+
+			if(i % columnNumber == 0) then
+				currentX = gridX
+				currentY = currentY + photoHeight + paddingY
+			end
+
+			MultiTouch.activate(images[i], "move", "single");
+			images[i]:addEventListener(MultiTouch.MULTITOUCH_EVENT, imageDrag);
+
+		end
+	end
+
+	boxGroup = display.newGroup()
+	boxSize = 50
 
 	if level == 'easy' then
 		numberOfCategories = 2
-		rand = randomizeCategory()
+		gameCategories = randomizeCategory()
 
 		-- boxes
-		box1.x = width/4; box1.y = 290
-		box2.x = width/4 + (4*boxSize); box2.y = 290
-		display.newText(categories[rand[1]], box1.x-20, box1.y, font, 20)
-		display.newText(categories[rand[2]], box2.x-20, box2.y, font, 20)
-		boxGroup:insert(box1)
-		boxGroup:insert(box2)
+		box = {}
+		for i = 1, numberOfCategories do
+			box[i] = display.newImageRect("images/secondgame/box.png", 80, 80)
+		end
+		box[1].x = width/4; box[1].y = 290
+		box[2].x = width/4 + (4*boxSize); box[2].y = 290
 
-		--randomize correct answers
+		for i = 1, numberOfCategories do
+			display.newText(categories[gameCategories[i]], box[i].x-20, box[i].y, font, 20)
+			boxGroup:insert(box[i])
+		end
+
 		allWords = getWords("correct")
 		numberOfCorrectAnswers = 14
-		correctWords = randomize(allWords, numberOfCorrectAnswers)
-		for i=1,#correctWords do
-			print(correctWords[i])
-		end
+		selectedWords = randomize(allWords, numberOfCorrectAnswers)
+		print("SELECTION")
+		for i=1,#selectedWords do print(selectedWords[i]) end
 
 		--randomize panggulo
 		allExtras = getWords("incorrect")
 		numberOfIncorrectAnswers = 10
 		extraWords = randomize(allExtras, numberOfIncorrectAnswers)
-		for i=1,#extraWords do
-			print(extraWords[i])
-		end
+--		for i=1,#extraWords do print(extraWords[i]) end
 
 		-- pictures
+		photos = {}
 		length = 24
 		for i = 1, length do
-			easy[i] = "images/secondgame/game1.png"
+			photos[i] = "images/secondgame/game1.png"
 		end
 
-		--display board
-		for i = 1, numberOfCorrectAnswers do easyText[i] = correctWords[i] end
-		for i = numberOfCorrectAnswers+1, length do easyText[i] = extraWords[i - numberOfCorrectAnswers] end
+		-- labels
+		word = {}
+		for i = 1, numberOfCorrectAnswers do word[i] = selectedWords[i] end
+		for i = numberOfCorrectAnswers+1, length do word[i] = extraWords[i - numberOfCorrectAnswers] end
+		word = shuffle(word)
+
+		print("TO DISPLAY!!!!")
+		for i = 1, length do
+			print(word[i])
+		end
 
 		--Initialize the starView object. The parameters are the gridX, gridY, photoArray, photoTextArray, columnNumber, paddingX, paddingY, photoWidth, photoHeight, gridListener.
-		gridView:new(width/7, 30, easy, easyText, length/4, 5, 5, 50, 50, gridListener)
+		drawGrid(width/7, 30, photos, word, length/4, 5, 5, 50, 50)
 
 	elseif level == 'medium' then
-		-- boxes
 		numberOfCategories = 3
-		rand = randomizeCategory()
+		gameCategories = randomizeCategory()
+		box = {}
 
-		box1.x = width/3 - boxSize; box1.y = 290
-		box2.x = width/3 + boxSize; box2.y = 290
-		box3 = display.newImageRect("images/secondgame/box.png", 80, 80)
-		box3.x = width/3 + (3*boxSize); box3.y = 290
-		display.newText(categories[rand[1]], box1.x-20, box1.y, font, 20)
-		display.newText(categories[rand[2]], box2.x-20, box2.y, font, 20)
-		display.newText(categories[rand[3]], box3.x-20, box3.y, font, 20)
-		boxGroup:insert(box1)
-		boxGroup:insert(box2)
-		boxGroup:insert(box3)
+		--BOXES
+		for i = 1, numberOfCategories do
+			box[i] = display.newImageRect("images/secondgame/box.png", 80, 80)
+		end
+
+		box[1].x = width/3 - boxSize; box[1].y = 290
+		box[2].x = width/3 + boxSize; box[2].y = 290
+		box[3].x = width/3 + (3*boxSize); box[3].y = 290
+
+		for i = 1, numberOfCategories do
+			display.newText(categories[gameCategories[i]], box[i].x-20, box[i].y, font, 20)
+			boxGroup:insert(box[i])
+		end
 
 		-- * CHOOSE WORDS  * --
 
 		-- correct answers
 		allWords = getWords("correct")
 		numberOfCorrectAnswers = 17
-		correctWords = randomize(allWords, numberOfCorrectAnswers)
-		for i=1,#correctWords do
-			print(correctWords[i])
-		end
+		selectedWords = randomize(allWords, numberOfCorrectAnswers)
+--		for i=1,#correctWords do print(correctWords[i]) end
 
 		-- panggulo
 		allExtras = getWords("incorrect")
 		numberOfIncorrectAnswers = 15
 		extraWords = randomize(allExtras, numberOfIncorrectAnswers)
-		for i=1,#extraWords do
-			print(extraWords[i])
-		end
+--		for i=1,#extraWords do print(extraWords[i]) end
 
 		-- * DISPLAY * --
 
 		-- pictures
+		photos = {}
 		length = 32
 		for i = 1, length do
-			med[i] = "images/secondgame/game1.png"
+			photos[i] = "images/secondgame/.png"
 		end
 
-		-- text
-		for i = 1, numberOfCorrectAnswers do medText[i] = correctWords[i] end
-		for i = numberOfCorrectAnswers+1, length do medText[i] = extraWords[i - numberOfCorrectAnswers] end
+		-- labels
+		word = {}
+		for i = 1, numberOfCorrectAnswers do word[i] = selectedWords[i] end
+		for i = numberOfCorrectAnswers+1, length do word[i] = extraWords[i - numberOfCorrectAnswers] end
+		word = shuffle(word)
 
 		--Initialize the starView object. The parameters are the gridX, gridY, photoArray, photoTextArray, columnNumber, paddingX, paddingY, photoWidth, photoHeight, gridListener.
-		gridView:new(width/22, 30, med, medText, length/4, 5, 5, 50, 50, gridListener)
+		drawGrid(width/22, 30, photos, word, length/4, 5, 5, 50, 50)
 
 	else
-		-- boxes
-		numberOfCategories = 4
-		rand = randomizeCategory()
-		box1.x = width/4 - boxSize; box1.y = 290
-		box2.x = width/4 + boxSize; box2.y = 290
-		box3 = display.newImageRect("images/secondgame/box.png", 80, 80)
-		box3.x = width/4 + (3*boxSize); box3.y = 290
-		box4 = display.newImageRect("images/secondgame/box.png", 80, 80)
-		box4.x = width/4 + (5*boxSize); box4.y = 290
-		display.newText(categories[rand[1]], box1.x-20, box1.y, font, 20)
-		display.newText(categories[rand[2]], box2.x-20, box2.y, font, 20)
-		display.newText(categories[rand[3]], box3.x-20, box3.y, font, 20)
-		display.newText(categories[rand[4]], box4.x-20, box4.y, font, 20)
-		boxGroup:insert(box1)
-		boxGroup:insert(box2)
-		boxGroup:insert(box3)
-		boxGroup:insert(box4)
 
+		numberOfCategories = 4
+		gameCategories = randomizeCategory()
+
+		box = {}
+		--BOXES
+		for i = 1, numberOfCategories do
+			box[i] = display.newImageRect("images/secondgame/box.png", 80, 80)
+		end
+
+		box[1].x = width/4 - boxSize; box[1].y = 290
+		box[2].x = width/4 + boxSize; box[2].y = 290
+		box[3].x = width/4 + (3*boxSize); box[3].y = 290
+		box[4].x = width/4 + (5*boxSize); box[4].y = 290
+
+		for i = 1, numberOfCategories do
+			display.newText(categories[gameCategories[i]], box[i].x-20, box[i].y, font, 20)			
+			boxGroup:insert(box[i])
+		end
 
 		-- * CHOOSE WORDS  * --
 
 		-- correct answers
 		allWords = getWords("correct")
 		numberOfCorrectAnswers = 24
-		correctWords = randomize(allWords, numberOfCorrectAnswers)
-		for i=1,#correctWords do
-			print(correctWords[i])
-		end
+		selectedWords = randomize(allWords, numberOfCorrectAnswers)
+--		for i=1,#correctWords do print(correctWords[i]) end
 
 		-- panggulo
 		allExtras = getWords("incorrect")
@@ -279,17 +460,20 @@ function scene:createScene(event)
 		-- * DISPLAY * --
 
 		-- pictures
+		photos = {}
 		length = 40
 		for i = 1, length do
-			hard[i] = "images/secondgame/game1.png"
+			photos[i] = "images/secondgame/game1.png"
 		end
 
-		-- text
-		for i = 1, numberOfCorrectAnswers do hardText[i] = correctWords[i] end
-		for i = numberOfCorrectAnswers+1, length do hardText[i] = extraWords[i - numberOfCorrectAnswers] end
+		-- labels
+		word = {}
+		for i = 1, numberOfCorrectAnswers do word[i] = selectedWords[i] end
+		for i = numberOfCorrectAnswers+1, length do word[i] = extraWords[i - numberOfCorrectAnswers] end
+		word = shuffle(word)
 
 		--Initialize the starView object. The parameters are the gridX, gridY, photoArray, photoTextArray, columnNumber, paddingX, paddingY, photoWidth, photoHeight, gridListener.
-		gridView:new(-30, 30, hard, hardText, length/4, 5, 5, 50, 50, gridListener)
+		drawGrid(-30, 30, photos, word, length/4, 5, 5, 50, 50)
 
 	end
 end
