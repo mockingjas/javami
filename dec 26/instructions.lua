11b3884805ce996eac9867b487b1d03471219c70
Jasmine <jasmtolentino@gmail.com>
2013-12-26 22:04:06 +0800

    dec 26 folder = JAVAMI + javami_dec1_vanny

---
+++ dec 26/instructions.lua
@@ -0,0 +1,39 @@
+require "physics"
+local storyboard = require ("storyboard")
+local widget= require ("widget")
+local scene = storyboard.newScene()
+local gameScreen, tabGroup
+
+function scene:createScene( event )
+
+	local slideView = require("slideView")
+	
+	local myImages = {
+		"images/menu/howtoplay1.png",
+		"images/menu/howtoplay2.png",
+		"images/menu/howtoplay3.png",
+	}		
+
+	slideView.new( myImages )	
+
+
+end
+
+function scene:enterScene( event )
+
+end
+
+function scene:exitScene( event )
+
+end
+
+function scene:destroyScene( event )
+
+end
+
+scene:addEventListener( "createScene", scene )
+scene:addEventListener( "enterScene", scene )
+scene:addEventListener( "exitScene", scene )
+scene:addEventListener( "destroyScene", scene )
+
+return scene
