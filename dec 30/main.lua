2af3b006beb449baf99915ec0fdd58456fd0d497
Vanny Conoza <vanessavielbconoza@gmail.com>
2013-12-30 21:52:06 +0800

    Dec 30 folder = Fixed some bugs, graphics and music

---
+++ dec 30/main.lua
@@ -0,0 +1,15 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+require "sqlite3"
+local lfs = require "lfs"
+local path = system.pathForFile("JaVaMiaDb.sqlite3", system.ResourceDirectory)
+db = sqlite3.open( path )   
+local file
+local option
+local storyboard = require "storyboard"
+
+storyboard.gotoScene( "startmenu")
---
+++ dec 30/reloadscores.lua
@@ -0,0 +1,38 @@
+
+local storyboard = require ("storyboard")
+local widget = require( "widget" )
+local scene = storyboard.newScene()
+
+function scene:createScene(event)
+	
+end
+
+function scene:enterScene(event)
+	storyboard.removeScene("scoreboard")
+
+	mainMusic = audio.loadSound("music/MainSong.mp3")
+	backgroundMusicChannel = audio.play( mainMusic, { loops=-1}  )
+	option =	{
+		effect = "fade",
+		time = 400,
+		params = {
+			music = backgroundMusicChannel
+		}
+	}
+	storyboard.gotoScene("mainmenu", option)
+end
+
+function scene:exitScene(event)
+	
+end
+
+function scene:destroyScene(event)
+
+end
+
+scene:addEventListener("createScene", scene)
+scene:addEventListener("enterScene", scene)
+scene:addEventListener("exitScene", scene)
+scene:addEventListener("destroyScene", scene)
+
+return scene
