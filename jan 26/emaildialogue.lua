dcd360138a8a3fce77121860ee24b382dd7613e1
Maricia <mariciabalayan@yahoo.com>
2014-01-27 17:06:04 +0800

    Email with attachment (temp values)

--- jan 26/emaildialogue.lua
+++ jan 26/emaildialogue.lua
@@ -1,6 +1,6 @@
 require "physics"
 local storyboard = require ("storyboard")
-local widget= require ("widget")
+local widget = require( "widget" )
 local scene = storyboard.newScene()
 
 -- Level Select Modal Variables --
@@ -17,42 +17,33 @@ else
     -- Mac and iOS
     font = "Eraser-Regular"
 end
- 
--- TextField Listener
-local function fieldHandler( getObj )
-        
--- Use Lua closure in order to access the TextField object
- 
-        return function( event )
- 
-                print( "TextField Object is: " .. tostring( getObj() ) )
-                
-                if ( "began" == event.phase ) then
-                        -- This is the "keyboard has appeared" event
-                
-                elseif ( "ended" == event.phase ) then
-                        -- This event is called when the user stops editing a field:
-                        -- for example, when they touch a different field or keyboard focus goes away
-                
-                        print( "Text entered = " .. tostring( getObj().text ) )         -- display the text entered
-                        namedisplay.text = name.text
-                        agedisplay.text = age.text
-                        emaildisplay.text = email.text
-                        
-                elseif ( "submitted" == event.phase ) then
-                        -- This event occurs when the user presses the "return" key
-                        -- (ifable) on the onscreen keyboard
-                        --namedisplay.text = name.text
-                        --agedisplay.text = age.text
-                        --emaildisplay.text = email.text
-                        -- Hide keyboard
-                        native.setKeyboardFocus( nil )
-                end
-                
-        end     -- "return function()"
- 
+
+--temp
+local userAge = 12
+local username = "Cha"
+local emailaddress = "mariciabalayan@gmail.com"
+
+local function onSendEmail( event )
+	print("sdklfksdlf")
+	local options =
+	{
+	   to = emailaddress,
+	   subject = "Game Analytics",
+	   body = "Name: "..username.."/nAge: "..userAge,
+	   attachment = { baseDir=system.ResourceDirectory, filename="Game 1 Analytics.txt", type="text" },
+	}
+	print(native.showPopup("mail", options))
+	native.showPopup("mail", options)
 end
- 
+
+local function textListener( event )
+	if(event.phase == "began") then
+	elseif(event.phase == "editing") then
+	elseif(event.phase == "ended") then
+		name.text = event.target.text
+	end
+end
+
 function scene:createScene(event)
 
 	local screenGroup = self.view
@@ -67,59 +58,49 @@ function scene:createScene(event)
 	-- Create our Text Field
 	namelabel = display.newText("Kid's name", 50, 100, font, 25)
 	namelabel:setTextColor(0,0,0)
-	
 	name = native.newTextField( 220, 100, 180, 30 )    -- passes the text field object
     name:setTextColor( 255,255,255)
-    name.hasBackground = "false"
-	name.hintText = "Enter kid's name"
-	name.text = name.hintText
-
-	namedisplay = display.newText("", 230, 103, native.systemFont, 22)
-	namedisplay:setTextColor(0,0,0)
+    name.hasBackground = false
+    name.hintText = "Ross Geller"
+   	name.text = name.hintText
    	screenGroup:insert(namelabel)
    	screenGroup:insert(name)
-   	screenGroup:insert(namedisplay)
 
    	agelabel = display.newText("Kid's Age", 50, 140, font, 25)
    	agelabel:setTextColor(0,0,0)
 	age = native.newTextField( 220, 140, 100, 30 )    -- passes the text field object
     age:setTextColor( 255,255,255)
+    age.hasBackground = false
    	age.inputType = "number"
+   	age.hintText = "5"
+   	age.text = age.hintText
 
-   	agedisplay = display.newText("", 230, 143, native.systemFont, 22)
-	agedisplay:setTextColor(0,0,0)
    	screenGroup:insert(agelabel)
    	screenGroup:insert(age)
-   	screenGroup:insert(agedisplay)
 
    	emaillabel = display.newText("Teacher/Parent's Email", 50, 180, font, 25)
    	emaillabel:setTextColor(0,0,0)
 	email = native.newTextField( 50, 210, 300, 30 )    -- passes the text field object
     email:setTextColor( 255,255,255)
+    email.hasBackground = false
    	email.inputType = "email"
-
-   	emaildisplay = display.newText("", 60, 215, native.systemFont, 22)
-	emaildisplay:setTextColor(0,0,0)
+   	email.hintText = "drgeller@friends.org"
+   	email.text = email.hintText
    	screenGroup:insert(emaillabel)
    	screenGroup:insert(email)
-   	screenGroup:insert(emaildisplay)
 
-   	submit = widget.newButton{
-		id = "submit",
+   	local sendEmail = widget.newButton{
 		defaultFile = "images/firstgame/submit_button.png",
 		fontSize = 15,
 		emboss = true,
-		--onEvent = checkanswer,
+		onRelease = onSendEmail
 	}
-	submit.x = 430; submit.y = 270
-	screenGroup:insert(submit)
+	sendEmail.x = 430; sendEmail.y = 270
+	screenGroup:insert(sendEmail)
 
-	name:addEventListener( "userInput", fieldHandler)
-	age:addEventListener( "userInput", fieldHandler)
-	email:addEventListener( "userInput", fieldHandler)
-
-
-
+	name:addEventListener( "userInput", textListener)
+	age:addEventListener( "userInput", textListener)
+	email:addEventListener( "userInput", textListener)
 
 end
 
