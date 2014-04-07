eb5b8979493c71b701fc536a82ba0b45178be5aa
Maricia <mariciabalayan@yahoo.com>
2014-04-07 18:59:56 +0800

    Emptied scores DB

--- SkillVille/main.lua
+++ SkillVille/main.lua
@@ -11,8 +11,8 @@ db = sqlite3.open( path )
 
 storyboard.gotoScene("StartMenu")
 
---[[for row in db:nrows("ALTER TABLE ThirdGameAnalytics RENAME TO GameOneAnalytics") do	print(row.name) end
-print("**DATABASE**")
+
+--[[print("**DATABASE**")
 for row in db:nrows("SELECT name FROM sqlite_master WHERE type='table';") do
 	print(row.name)
 end]]
